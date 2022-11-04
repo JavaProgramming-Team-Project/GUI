f20ddd3b0abcdcc9c6f60d0a5d8514b9c33135af
정주원 <filocd05155@naver.com>
2022-11-04 16:13:46 +0900

    테스트

--- src/ui/MainPageGUI.java
+++ src/ui/MainPageGUI.java
@@ -1,4 +1,37 @@
 package ui;
 
-public class MainPageGUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainPageGUI extends JFrame{
+    private JPanel MainPanel;
+    private HeadPanel headPanel;
+    private Container ct;
+    public MainPageGUI(){
+        ct = getContentPane();
+
+        MainPanel = new JPanel();
+        MainPanel.setLayout(null);
+
+        headPanel = new HeadPanel();
+        headPanel.setBounds(0,0,1000,100);
+
+        MainPanel.add(headPanel);
+
+        JLabel jl = new JLabel("안녕안녕");
+        jl.setBounds(100,100,100,100);
+
+        MainPanel.add(jl);
+
+        ct.add(MainPanel);
+
+        setVisible(true);
+        setSize(1000,700);
+        setTitle("EVERY BOOK");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+    public static void main(String[] args){
+        new MainPageGUI();
+    }
 }
