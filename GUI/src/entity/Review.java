076ab986f03272f925c43bb1553bdcd3086076a2
정주원 <filocd05155@naver.com>
2022-11-20 20:34:46 +0900

    리뷰 엔티티 추가

---
+++ GUI/src/entity/Review.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class Review {
+    private Member member;
+    private Item item;
+    private int ReviewId;
+    private Long MemberId;
+    private int star;
+    private String body;
+    private String date;
+
+    public Review(){
+        
+    }
+}
