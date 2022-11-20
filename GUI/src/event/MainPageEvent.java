f4c99a19f1546a79aaa5988098f214bdcad81fad
정주원 <filocd05155@naver.com>
2022-11-21 00:07:06 +0900

    ..

--- GUI/src/event/MainPageEvent.java
+++ GUI/src/event/MainPageEvent.java
@@ -17,7 +17,7 @@ public class MainPageEvent extends JPanel{
         Color SoftBlue = new Color(0xB8E9FF);
 
         // 데이터 가져오기
-        CategoryList = item.getCategory();
+        //CategoryList = item.getCategory();
 
         // 패널 설정
         ItemPanel = new JPanel();
@@ -25,20 +25,20 @@ public class MainPageEvent extends JPanel{
         ItemPanel.setBackground(SoftBlue);
 
         // 라벨 설정
-        JLabel CategoryName = new JLabel("["+CategoryList[0]+"]");
-        CategoryName.setFont(new Font("맑은 고딕",Font.BOLD, 11));
-        CategoryName.setFont(CategoryName.getFont().deriveFont(21.0f));
-        CategoryName.setBounds(0,0,100,100);
-        CategoryName.setForeground(Color.gray);
+        //JLabel CategoryName = new JLabel("["+CategoryList[0]+"]");
+        //CategoryName.setFont(new Font("맑은 고딕",Font.BOLD, 11));
+        //CategoryName.setFont(CategoryName.getFont().deriveFont(21.0f));
+        //CategoryName.setBounds(0,0,100,100);
+        //CategoryName.setForeground(Color.gray);
 
-        JLabel ItemName = new JLabel(item.getName());
-        JLabel Body = new JLabel(item.getBody());
-        JLabel Price = new JLabel(item.getPrice());
+        //JLabel ItemName = new JLabel(item.getName());
+        //JLabel Body = new JLabel(item.getBody());
+        //JLabel Price = new JLabel(item.getPrice());
 
         // 패널 추가
-        ItemPanel.add(CategoryName);
-        ItemPanel.add(ItemName);
+        //ItemPanel.add(CategoryName);
+        //ItemPanel.add(ItemName);
         //ItemPanel.add(Body);
-        ItemPanel.add(Price);
+        //ItemPanel.add(Price);
     }
 }
