fb2f58891695e004ac1b26d321befc2f86e0e16d
정주원 <filocd05155@naver.com>
2022-11-27 14:05:55 +0900

    상품패널 연동

--- GUI/src/event/CategoryPageEvent.java
+++ GUI/src/event/CategoryPageEvent.java
@@ -23,71 +23,64 @@ public class CategoryPageEvent extends JPanel implements MouseListener {
     public void mouseClicked(MouseEvent e) {
         Color SoftBlue = new Color(0xB8E9FF);;
         if(e.getSource() == cgp.StayLabel){
-            System.out.println("haha");
             Remove_highlights();
-            cgp.ItemDisplay("숙박",0);
+            cgp.itemDisplay("숙박",0);
             cgp.StayLabel.setOpaque(true);
             cgp.StayLabel.setBackground(SoftBlue);
         }
         else if(e.getSource() == cgp.LeisureLabel){
             Remove_highlights();
-            cgp.ItemDisplay("레저",0);
+            cgp.itemDisplay("레저",0);
             cgp.LeisureLabel.setOpaque(true);
             cgp.LeisureLabel.setBackground(SoftBlue);
         }
         else if(e.getSource() == cgp.FestivalLabel){
             Remove_highlights();
-            cgp.ItemDisplay("축제",0);
+            cgp.itemDisplay("축제",0);
             cgp.FestivalLabel.setOpaque(true);
             cgp.FestivalLabel.setBackground(SoftBlue);
         }
-        else if(e.getSource() == cgp.FestivalLabel){
+        else if(e.getSource() == cgp.ShowLabel){
             Remove_highlights();
-            cgp.ItemDisplay("공연",0);
+            cgp.itemDisplay("공연",0);
             cgp.ShowLabel.setOpaque(true);
             cgp.ShowLabel.setBackground(SoftBlue);
         }
         else if(e.getSource() == cgp.DisplayLabel){
             Remove_highlights();
-            cgp.ItemDisplay("전시",0);
+            cgp.itemDisplay("전시",0);
             cgp.DisplayLabel.setOpaque(true);
             cgp.DisplayLabel.setBackground(SoftBlue);
         }
         else if(e.getSource() == cgp.TicketLabel){
             Remove_highlights();
-            cgp.ItemDisplay("티켓",0);
+            cgp.itemDisplay("티켓",0);
             cgp.TicketLabel.setOpaque(true);
             cgp.TicketLabel.setBackground(SoftBlue);
         }
         else if(e.getSource() == cgp.RestaurantLabel){
             Remove_highlights();
-            cgp.ItemDisplay("식당",0);
+            cgp.itemDisplay("식당",0);
             cgp.RestaurantLabel.setOpaque(true);
             cgp.RestaurantLabel.setBackground(SoftBlue);
         }
         else if(e.getSource() == cgp.BeautyLabel){
             Remove_highlights();
-            cgp.ItemDisplay("뷰티",0);
+            cgp.itemDisplay("뷰티",0);
             cgp.BeautyLabel.setOpaque(true);
             cgp.BeautyLabel.setBackground(SoftBlue);
         }
         else if(e.getSource() == cgp.ItemPanel){
             JPanel jp = (JPanel)e.getSource();
-            cgp.visitProduct(jp);
-
-            /*
+            cgp.MainPanel.remove(cgp.ProductPanel2);
             cgp.productPanelGUI = new ProductPanelGUI(cgp.itemList, Integer.parseInt(jp.getName()));
             cgp.ProductPanel2 = cgp.productPanelGUI.MainPanel;
-            cgp.ProductPanel2.setBounds(0,0,1300,680);
-            JLabel jl = new JLabel("하이하이");
-            jl.setBounds(0,0,100,100);
-            cgp.ProductPanel2.add(jl);
-            cgp.ProductPanel2.setVisible(true);
+            cgp.ProductPanel2.setBounds(100,0,1200,680);
+            cgp.ProductPanel2.setLayout(null);
+            cgp.ProductPanel2.setBackground(Color.WHITE);
+            cgp.MainPanel.add(cgp.ProductPanel2);
             cgp.CategoryPanel.setVisible(false);
-            cgp.ProductPanel2.repaint();
-            cgp.MainPanel.repaint();
-
-             */
+            cgp.ProductPanel2.setVisible(true);
         }
     }
 
